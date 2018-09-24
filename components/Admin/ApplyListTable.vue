7e573d75b6de1d0ba6fc5a1e1dd6d887e3d4ced7
choiteageun <pioneer2001@naver.com>
2018-09-25 01:24:24 +0900

    중앙처리세스 배우기 전

---
+++ components/Admin/ApplyListTable.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+    <el-table :data="applyList" size="mini">
+      <el-table-column type="selection"></el-table-column>
+      <el-table-column prop="name" label="고객명"></el-table-column>
+      <el-table-column prop="tel" label="연락처"></el-table-column>
+      <el-table-column prop="loanAmount" label="신청금액"></el-table-column>
+      <el-table-column prop="route" label="경로"></el-table-column>
+      <el-table-column label="담당자">
+        <template slot-scope="scope">
+          <el-select v-model="scope.row.manager" value-key="scope.row.id">
+            <el-option label="최태근부장" value="최태근부장"></el-option>
+            <el-option label="나일등대리" value="나일등대리"></el-option>
+          </el-select>
+        </template>
+      </el-table-column>
+      <el-table-column width="160">
+        <template slot-scope="scope">
+          <el-button size="mini" @click=" dialog.updateConsul = true">상세정보</el-button>
+          <el-button type="primary" size="mini">지정</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div class="pagination center">
+      <el-pagination background layout="prev, pager, next" :total="1000"></el-pagination>
+    </div>
+    <div class="bottom-btn-group" style="text-align:right;margin-top:20px;">
+      <el-button @click="dialog.createCustomer = true">고객등록</el-button>
+      <el-button @click="dialog.createStaff = true">직원등록</el-button>
+      <el-select v-model="selected" value-key="selected">
+        <el-option label="최태근부장" value="1"></el-option>
+        <el-option label="나일등대리" value="2"></el-option>
+      </el-select>
+      <el-button type="primary">이동</el-button>
+    </div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  data() {
+  },
+
+  props: {
+    dialog: {
+      type: Object,
+      required: true
+    },
+
+    applyList:{
+      type: Array,
+      required:true
+    },
+  },
+  async mounted() {
+    this.$emit("initConsul")
+  }
+};
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ pages/admin.vue
@@ -0,0 +1,48 @@
+<template>
+  <section>
+    <div class="center">
+      <h1 class="title">Final Admin Page</h1>
+    </div>
+    <div class="container flex topBtns">
+      <el-button @click="tab = 'apply'" :type="tab === 'apply' ? 'primary' : 'warning'" >신청목록</el-button>
+      <el-button @click="tab = 'complete'" :type="tab === 'complete' ? 'primary' : 'warning'">승인목록</el-button>
+      <el-button @click="tab = 'notice'" :type="tab === 'notice' ? 'primary' : 'warning'">공지사항</el-button>
+      <el-button @click="tab = 'customer'" :type="tab === 'customer' ? 'primary' : 'warning'">고객관리</el-button>
+      <el-button @click="tab = 'staff'" :type="tab === 'staff' ? 'primary' : 'warning'" >직원관리</el-button>
+    </div>
+    <div>
+      <ApplyList v-if="tab === 'apply'"></ApplyList>
+      <CompleteList v-if="tab === 'complete'"></CompleteList>
+      <Customer v-if="tab === 'notice'"></Customer>
+      <Notice v-if="tab === 'customer'"></Notice>
+      <Staff v-if="tab === 'staff'"></Staff>
+    </div>
+  </section>
+</template>
+<script>
+import ApplyList from "@/components/Admin/ApplyList.vue"
+import CompleteList from "@/components/Admin/CompleteList.vue"
+import Customer from "@/components/Admin/Customer.vue"
+import Notice from "@/components/Admin/Notice.vue"
+import Staff from "@/components/Admin/Staff.vue"
+export default {
+  data() {
+    return {
+      tab: "apply",
+    };
+  },
+  components:{
+    ApplyList, CompleteList, Customer, Notice, Staff
+  }
+};
+</script>
+<style lang="scss" scoped>
+.topBtns {
+  margin-top: 15px;
+  margin-bottom: 15px;
+}
+.title {
+  font-size: 40px;
+  margin: 20px 0 40px 0;
+}
+</style>
